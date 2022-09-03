a0e6af8bcbee4561df78bacead3b845ac934ea24
Merdan Charliev <mcharliev@gmail.com>
2022-09-03 16:47:58 +0300

    CourseWork!

---
+++ src/firstCourseWork/Employee.java
@@ -0,0 +1,7 @@
+package firstCourseWork;
+
+public class Employee {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/firstCourseWork/Store.java
@@ -0,0 +1,10 @@
+package firstCourseWork;
+
+import java.util.Arrays;
+
+public class Store {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
+
